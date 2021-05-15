03162e265da704628f425bbd13f0884a20598114
Irfani-ardhyansah <mohamadfani99@gmail.com>
2021-05-15 10:21:08 +0700

    Add Link Linkedin At Footer

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
   <div class="footer-left">
-    Copyright &copy; 2021 <div class="bullet"></div> Develop By <a href="https://gitlab.com/Irfani-ardhyansah/">Mochamad Irfani Ardhyansah</a>
+    Copyright &copy; 2021 <div class="bullet"></div> Develop By <a href="https://www.linkedin.com/in/irfani-ardhyansah-1546b2211/" target="_blank`">Mochamad Irfani Ardhyansah</a>
   </div>
   <div class="footer-right">
     1.0
